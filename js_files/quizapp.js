5c949dcb17fef87ab0a5b54291e1ad864ad9e95c
Alexander <maina206mwangi@gmail.com>
2024-11-18 14:50:41 +0300

    added the Quiz App

---
+++ js_files/quizapp.js
@@ -0,0 +1,123 @@
+const questions = [
+  {
+    question: "What is the largest animal in the world?",
+    answers: [
+      { text: "Shark", correct: false },
+      { text: "Blue Whale", correct: true },
+      { text: "Elephant", correct: false },
+      { text: "Giraffe", correct: false },
+    ],
+  },
+  {
+    question: "What is the Capital City of Kenya?",
+    answers: [
+      { text: "Mombasa", correct: false },
+      { text: "Nakuru", correct: false },
+      { text: "Kisumu", correct: false },
+      { text: "Nairobi", correct: true },
+    ],
+  },
+  {
+    question: "Who was the second President of Kenya?",
+    answers: [
+      { text: "Mwai Kibaki", correct: false },
+      { text: "Daniel Arap Moi", correct: true },
+      { text: "Jomo Kenyatta", correct: false },
+      { text: "Uhuru Kenyatta", correct: false },
+    ],
+  },
+  {
+    question: "Which is the most driven car in Kenya?",
+    answers: [
+      { text: "Toyota Land Cruiser Prado TX", correct: true },
+      { text: "Toyota RAV4", correct: false },
+      { text: "Subaru Forester", correct: false },
+      { text: "Toyota Corolla Fielder", correct: false },
+    ],
+  },
+];
+
+const questionElement = document.getElementById("question");
+const answerButtons = document.getElementById("answer-buttons");
+const nextButton = document.getElementById("next-btn");
+
+let currentQuestionIndex = 0;
+let score = 0;
+
+function startQuiz() {
+  currentQuestionIndex = 0;
+  score = 0;
+  nextButton.innerHTML = "Next";
+  showQuestion();
+}
+
+function showQuestion() {
+  resetState();
+
+  let currentQuestion = questions[currentQuestionIndex];
+  let questionNo = currentQuestionIndex + 1;
+  questionElement.innerHTML = questionNo + ". " + currentQuestion.question;
+
+  currentQuestion.answers.forEach((answer) => {
+    const button = document.createElement("button");
+    button.innerHTML = answer.text;
+    button.classList.add("btn");
+    answerButtons.appendChild(button);
+
+    if (answer.correct) {
+      button.dataset.correct = answer.correct;
+    }
+    button.addEventListener("click", selectAnswer);
+  });
+}
+
+function resetState() {
+  nextButton.style.display = "none";
+  while (answerButtons.firstChild) {
+    answerButtons.removeChild(answerButtons.firstChild);
+  }
+}
+
+function selectAnswer(e) {
+  const selectedBtn = e.target;
+  const isCorrect = selectedBtn.dataset.correct === "true";
+  if (isCorrect) {
+    selectedBtn.classList.add("correct");
+    score++;
+  } else {
+    selectedBtn.classList.add("incorrect");
+  }
+  Array.from(answerButtons.children).forEach((button) => {
+    if (button.dataset.correct === "true") {
+      button.classList.add("correct");
+    }
+    button.disabled = true;
+  });
+  nextButton.style.display = "block";
+}
+
+function showScore() {
+  resetState();
+  questionElement.innerHTML = `You scored ${score} out of ${questions.length}`;
+  nextButton.innerHTML = "Restart";
+  nextButton.style.display = "block";
+}
+
+function handleNextButton() {
+  currentQuestionIndex++;
+  if (currentQuestionIndex < questions.length) {
+    showQuestion();
+  } else {
+    showScore();
+  }
+}
+
+nextButton.addEventListener("click", () => {
+  if (currentQuestionIndex < questions.length) {
+    handleNextButton();
+  } else {
+    startQuiz();
+  }
+});
+
+startQuiz();
